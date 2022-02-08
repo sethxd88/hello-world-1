416e3aa157eeeb95fe3da8918d287e08d3a809bf
Seth <sepx898508@gmail.com>
2022-02-08 12:16:43 +0800

    feat: add date picker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,5 @@
-//= require jquery-3.2.1.slim.min
-//= require popper.min
-//= require bootstrap.min
-//= require bootstrap-datepicker/core
-//= require bootstrap-datepicker/locales/bootstrap-datepicker.en-GB.js
-
-$(document).ready(function(){
-  $('.datepicker').datepicker({
-    format: 'dd/mm/yyyy'
-  });
-});
+//= require jquery
+//= require rails-ujs
+//= require bootstrap-datepicker
+//= require turbolinks
+//= require_tree .
